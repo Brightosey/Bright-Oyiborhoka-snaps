ed3e89d4c74e1d16f2399714e3f7cf7d0104e5c2
Brightosey <Brightosey@users.noreply.github.com>
2025-02-15 14:25:10 -0500

    I refactored the Filters and Header components to implement hover effects on specific elements. The styling was updated to enhance the visual feedback, ensuring that interactive elements like filter tags and buttons provide clear user interaction cues. This update improves the overall user experience by making the UI more intuitive and visually responsive to mouse hover actions.

--- src/components/filters/filters.jsx
+++ src/components/filters/filters.jsx
@@ -1,13 +1,34 @@
 import "../filters/filters.scss";
 import Data from "../../Data/tags.json";
+import react, { useState } from "react";
+
 function Filter() {
+  const [isMousedOver, setMouseOver] = useState(false);
+
+  function handleMouseOver() {
+    setMouseOver(true);
+  }
+
+  function handleMouseOut() {
+    setMouseOver(false);
+  }
+
   return (
     <>
       <section className="filter">
         <h3 className="filter__header">Filters</h3>
         <div className="filter__list">
           {Data.map((item, index) => {
-            return <p className="filter__item" key={index}>{item}</p>;
+            return (
+              <p 
+              className="filter__item"
+              key={index}
+              onMouseOver={handleMouseOver}
+              onMouseOut={handleMouseOut}
+              >
+                {item}
+              </p>
+            );
           })}
         </div>
       </section>
